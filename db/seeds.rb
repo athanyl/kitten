ac926f3f1823ef43d4ff48da317ae6a64be0671e
athanyl <athanyl@gmail.com>
2018-11-26 17:54:50 +0100

    table items & seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+i = 1
+
+79.times do
+  if i > 9
+  	desc = Faker::Creature::Cat.breed << " - " << Faker::Creature::Cat.registry
+  	url = "http://thp.athanyl.net/kitten/kitten00" << i.to_s << ".jpg"
+  	thumburl = "http://thp.athanyl.net/kitten/thumb/thumb00" << i.to_s << ".jpg"
+  	myitems = Item.create!(title: Faker::Creature::Cat.name, description: desc, price: 0.01, image_url: url, thumb_url: thumburl)
+	else
+  	desc = Faker::Creature::Cat.breed + " - " + Faker::Creature::Cat.registry
+  	url = "http://thp.athanyl.net/kitten/kitten000" << i.to_s << ".jpg"
+  	thumburl = "http://thp.athanyl.net/kitten/thumb/thumb000" << i.to_s << ".jpg"		
+		myitems = Item.create!(title: Faker::Creature::Cat.name, description: desc, price: 0.01, image_url: url, thumb_url: thumburl)
+	end
+	i += 1
+end
