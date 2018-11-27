8ef915366d96cf5885d3bec076953296e0f9beeb
athanyl <athanyl@gmail.com>
2018-11-27 16:36:14 +0100

    ajout modele order

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_27_130842) do
+ActiveRecord::Schema.define(version: 2018_11_27_140451) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,6 +39,20 @@ ActiveRecord::Schema.define(version: 2018_11_27_130842) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "items_orders", id: false, force: :cascade do |t|
+    t.bigint "order_id", null: false
+    t.bigint "item_id", null: false
+    t.index ["item_id", "order_id"], name: "index_items_orders_on_item_id_and_order_id"
+    t.index ["order_id", "item_id"], name: "index_items_orders_on_order_id_and_item_id"
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "order_id"
+    t.index ["order_id"], name: "index_orders_on_order_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -54,5 +68,6 @@ ActiveRecord::Schema.define(version: 2018_11_27_130842) do
   end
 
   add_foreign_key "carts", "users"
+  add_foreign_key "orders", "orders"
   add_foreign_key "users", "carts"
 end
