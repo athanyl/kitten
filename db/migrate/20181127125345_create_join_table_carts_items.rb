82ffb9cb2ea4b1139046aad525f7a42d2ea64818
Jolan <krashbook@gmail.com>
2018-11-27 14:33:35 +0100

    cleaning migration

--- db/migrate/20181127125345_create_join_table_carts_items.rb
+++ db/migrate/20181127125345_create_join_table_carts_items.rb
@@ -1,8 +1,8 @@
 class CreateJoinTableCartsItems < ActiveRecord::Migration[5.2]
   def change
     create_join_table :carts, :items do |t|
-      t.index [:cart_id, :item_id], index: true, foreign_key: true
-      t.index [:item_id, :cart_id], index: true, foreign_key: true
+      t.index [:cart_id, :item_id]
+      t.index [:item_id, :cart_id]
     end
   end
 end
