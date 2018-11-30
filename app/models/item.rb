b7b66a5dc65b622f4d554bcf4d7d5e094a4d04b1
Jolan <krashbook@gmail.com>
2018-11-30 09:55:57 +0100

    clean

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,6 @@
 class Item < ApplicationRecord
 	has_and_belongs_to_many :carts
 	has_and_belongs_to_many :orders
-	has_many :likes, dependent: :destroy
 
 	validates :title, length: { minimum: 3,
     too_short: "%{count} charactères minimum" 
