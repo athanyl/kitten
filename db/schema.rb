07bfb4d26b6b74732dde5934233e3c96bd282c7f
athanyl <yannis@athanyl.net>
2018-11-30 11:47:47 +0100

    promo

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_27_164247) do
+ActiveRecord::Schema.define(version: 2018_11_30_054146) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,6 +46,15 @@ ActiveRecord::Schema.define(version: 2018_11_27_164247) do
     t.index ["order_id", "item_id"], name: "index_items_orders_on_order_id_and_item_id"
   end
 
+  create_table "likes", force: :cascade do |t|
+    t.bigint "item_id"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["item_id"], name: "index_likes_on_item_id"
+    t.index ["user_id"], name: "index_likes_on_user_id"
+  end
+
   create_table "orders", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -69,6 +78,8 @@ ActiveRecord::Schema.define(version: 2018_11_27_164247) do
   end
 
   add_foreign_key "carts", "users"
+  add_foreign_key "likes", "items"
+  add_foreign_key "likes", "users"
   add_foreign_key "orders", "users"
   add_foreign_key "users", "carts"
 end
