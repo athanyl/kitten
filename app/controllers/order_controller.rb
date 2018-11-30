718df7aa6c033f075d255bf760ac45458215b335
Jolan <krashbook@gmail.com>
2018-11-30 05:48:51 +0100

    end order db pusher

--- app/controllers/order_controller.rb
+++ app/controllers/order_controller.rb
@@ -1,19 +1,16 @@
 class OrderController < ApplicationController
     before_action :require_login
 
-    #def create_order
-    #    def push_to(user)
-    #        user.orders << new_order
-    #        user.save
-    #    end
-    #    
-    #    new_order = Order.new
-    #    current_user.cart.items.each do | item |
-    #        new_order.items << item
-    #        new_order.save
-    #    end
-    #    push_to(current_user)
-    #end
+    def create_order
+        
+        new_order = Order.new
+        current_user.cart.items.each do | item |
+            new_order.items << item
+            new_order.save
+        end
+        current_user.orders << new_order
+        current_user.save
+     end
 
     def show
         redirect_to '/', alert: "Please register to show profil !" if current_user == nil
