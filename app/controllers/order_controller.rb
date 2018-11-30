ad2ff0d53132bdffab5ecf3f2ace17ea2e5e4719
Jolan <krashbook@gmail.com>
2018-11-30 06:10:31 +0100

    end pusher order

--- app/controllers/order_controller.rb
+++ app/controllers/order_controller.rb
@@ -10,11 +10,49 @@ class OrderController < ApplicationController
         end
         current_user.orders << new_order
         current_user.save
-     end
+        redirect_to '/profil', notice: "Wow such donations kthxbye <3"
+    end
 
     def show
-        redirect_to '/', alert: "Please register to show profil !" if current_user == nil
+        redirect_to '/profil', alert: "Please register to show profil !" if current_user == nil
     end
+
+    def new
+	end
+
+	def create
+	  @amount = params[:amount]
+
+	  @amount = @amount.gsub('$', '').gsub(',', '')
+
+	  begin
+	    @amount = Float(@amount).round(2)
+	  rescue
+	    flash[:error] = 'Charge not completed. Please enter a valid amount in USD ($).'
+	    redirect_to new_charge_path
+	    return
+	  end
+
+	  @amount = (@amount * 100).to_i # Must be an integer!
+
+	  if @amount < 100
+	    flash[:error] = 'Charge not completed. Donation amount must be at least $5.'
+	    redirect_to new_charge_path
+	    return
+	  end
+
+	  Stripe::Charge.create(
+	    :amount => @amount,
+	    :currency => 'eur',
+	    :source => params[:stripeToken],
+	    :description => 'Custom donation'
+	  )
+
+	  rescue Stripe::CardError => e
+	    flash[:error] = e.message
+	    redirect_to new_charge_path
+	end
+	#UserMailer.order_confirmation.deliver_now!
 
 private
 
