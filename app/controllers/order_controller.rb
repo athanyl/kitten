61c30df6c3af15bf37fa8f313164f03172439779
Jolan <krashbook@gmail.com>
2018-11-30 06:14:43 +0100

    done

--- app/controllers/order_controller.rb
+++ app/controllers/order_controller.rb
@@ -18,47 +18,51 @@ class OrderController < ApplicationController
     end
 
     def new
+        redirect_to '/profil'
 	end
 
-	def create
-	  @amount = params[:amount]
-
-	  @amount = @amount.gsub('$', '').gsub(',', '')
-
-	  begin
-	    @amount = Float(@amount).round(2)
-	  rescue
-	    flash[:error] = 'Charge not completed. Please enter a valid amount in USD ($).'
-	    redirect_to new_charge_path
-	    return
-	  end
-
-	  @amount = (@amount * 100).to_i # Must be an integer!
-
-	  if @amount < 100
-	    flash[:error] = 'Charge not completed. Donation amount must be at least $5.'
-	    redirect_to new_charge_path
-	    return
-	  end
-
-	  Stripe::Charge.create(
-	    :amount => @amount,
-	    :currency => 'eur',
-	    :source => params[:stripeToken],
-	    :description => 'Custom donation'
-	  )
-
-	  rescue Stripe::CardError => e
-	    flash[:error] = e.message
-	    redirect_to new_charge_path
-	end
-	#UserMailer.order_confirmation.deliver_now!
-
-private
-
-  def require_login
-    unless current_user
-      redirect_to new_user_session_path
+    def create
+        @amount = params[:amount]
+        
+        @amount = @amount.gsub('$', '').gsub(',', '')
+        
+        begin
+            @amount = Float(@amount).round(2)
+        rescue
+            flash[:error] = 'Charge not completed. Please enter a valid amount in USD ($).'
+            redirect_to new_charge_path
+            return
+        end
+        
+        @amount = (@amount * 100).to_i # Must be an integer!
+        
+        if @amount < 100
+            flash[:error] = 'Charge not completed. Donation amount must be at least $5.'
+            redirect_to new_charge_path
+            return
+        end
+        
+        Stripe::Charge.create(
+            :amount => @amount,
+            :currency => 'eur',
+            :source => params[:stripeToken],
+            :description => 'Custom donation'
+            )
+            
+        redirect_to '/profil'
+        rescue Stripe::CardError => e
+            flash[:error] = e.message
+            redirect_to new_charge_path
+        end
+        
+        UserMailer.order_confirmation(current_user).deliver_now!
+        
+        private
+        
+        def require_login
+            unless current_user
+                redirect_to new_user_session_path
+            end
+        end
     end
-  end
-end
+    
